5d61c62d78d3a928a96b211e7a8cc1cfbaf77af8
ltzone <ltzhou@sjtu.edu.cn>
2021-04-22 10:29:38 +0800

    EE447 HW2

---
+++ EE447/HW2/test.py
@@ -0,0 +1,14 @@
+K = 29
+E_table = []
+for i in range(K+1):
+    E_table.append([0 for j in range(K+1)])
+
+for i in range(K+1):
+    E_table[i][K] = 100
+
+for i in range(K-1,-1,-1):
+    for j in range(K-1,-1,-1):
+        E_table[i][j] = - 1 + 0.51 * E_table[i][j+1] + 0.49 * E_table[i+1][j]
+
+for i in range(K):
+    print(29-i, E_table[1+i][i])
